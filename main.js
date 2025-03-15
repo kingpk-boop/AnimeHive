a1fcfd213bf85347a4757b1f923fc118199ab478
Anime <oopoku166@gmail.com>
2025-03-15 05:40:01 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// var swiper = new Swiper(".home", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable:true,
+//     }
+
+// });
+
+const autosearch = document.getElementById(`autosearch`);
+autosearch.addEventListener(`keyup`,e =>{
+    let currentvalue = target.value();
+    console.log(currentvalue);
+    let auto = Document.querySelectorAll(`h3`);
+} )
---
+++ script.js
@@ -0,0 +1,54 @@
+// function toggleMenu() {
+//     const navbar = document.querySelector('.navbar');
+//     navbar.classList.toggle('active');
+// }
+
+// function performSearch() {
+//     const searchInput = document.getElementById('search-input').value;
+//     if (searchInput) {
+//         // Redirect to a search results page with the query
+//         window.location.href = `search_results.html?q=${encodeURIComponent(searchInput)}`;
+//     } else {
+//         alert("Please enter a search term.");
+//     }
+// }
+
+// console.log();
+
+// document.getElementById().textContent="",
+
+// let x = 5;
+// console.log(x);
+//  let year = 2008;
+//  let age = 17;
+//  let phone_number = 5532535;
+//  let firstName = "Emmanuel";
+//  let lastName = "Opoku;"
+//  let email = "kdnlsk@gmail.com";
+
+//  console.log(year);
+//  console.log(age);
+//  console.log(phone_number);
+
+//  console.log(`I am ${age} years old`);
+//  console.log(`I was born in the year ${year}`);
+//  console.log(typeof year);
+//  console.log(typeof age);
+//  console.log(firstName);
+//  console.log(lastName);
+
+//  console.log(`My name is ${firstName} ${lastName}`);
+//  console.log(`Your email is ${email}`)
+
+// let offline = false;
+// let online = true;
+// let no = false;
+// let yes = true;
+
+// console.log(typeof offline);
+// console.log(typeof online);
+
+// console.log(`You are online ${online}`);
+// console.log(`You are offline ${offline}`);
+
+// console.log(`Is that Benze for sale ${yes}`)
